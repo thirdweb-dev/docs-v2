e4c37cd8c717c4bed3b261b604f11eb5ce03619a
Manan Tank <manantankm@gmail.com>
2023-11-07 00:34:56 +0530

    plz work

--- src/app/api/search/extraction/consts.ts
+++ src/app/api/search/extraction/consts.ts
@@ -1,13 +1,2 @@
-import path from "path";
-
-export const NEXT_OUTPUT_FOLDER = path.join(process.cwd(), ".next/server/app");
-
-export const SERACH_CONTENT_JSON = path.join(
-	process.cwd(),
-	".data/search-content.json",
-);
-
-console.log({
-	NEXT_OUTPUT_FOLDER,
-	SERACH_CONTENT_JSON,
-});
+export { SERACH_CONTENT_JSON } from "../../../../../paths";
+export { NEXT_OUTPUT_FOLDER } from "../../../../../paths";
