3e40ab9713be954445a695579c393df14e28d711
Manan Tank <manantankm@gmail.com>
2023-11-07 00:48:04 +0530

    debug

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,4 +1,6 @@
 export async function register() {
+	console.log("register api cwd", process.cwd());
+
 	if (process.env.NEXT_RUNTIME === "nodejs") {
 		const { getSearchIndexes } = await import(
 			"./app/api/search/indexing/createIndex"
