3fb03cca8788b53f07cc82e531abc6c34a0bc460
Manan Tank <manantankm@gmail.com>
2023-11-08 04:06:10 +0530

    remove debug

--- scripts/createEmptySearchIndex.ts
+++ scripts/createEmptySearchIndex.ts
@@ -1,6 +1,9 @@
 import { existsSync } from "fs";
 import { writeFile } from "fs-extra";
 
+// This is required to load the searchIndex.json on vercel server
+// searchIndex.json needs to be exist in file system before we run `next build`
+
 async function main() {
 	if (!existsSync("./searchIndex.json")) {
 		writeFile("./searchIndex.json", `[]`);
