468c9a2bb2c6c8bcc609de53e00bfe9677178483
Manan Tank <manantankm@gmail.com>
2023-12-23 14:48:50 +0530

    fixes

--- src/components/Document/DocImage.tsx
+++ src/components/Document/DocImage.tsx
@@ -1,10 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-export function DocImage(props: React.ComponentProps<typeof Image>) {
-	const { alt, ...restProps } = props;
+export function DocImage(props: {
+	src: React.ComponentProps<typeof Image>["src"];
+	alt?: string;
+	className?: string;
+}) {
+	const { alt, src } = props;
+	const cls = cn("rounded-lg", props.className);
+
 	return (
 		<div className="my-6 flex justify-center rounded-lg border p-4">
-			<Image className="rounded-lg" alt={alt} {...restProps} />
+			{typeof src === "string" ? (
+				<img className={cls} alt={alt || ""} src={src} />
+			) : (
+				<Image className={cls} alt={alt || ""} src={src} />
+			)}
 		</div>
 	);
 }
