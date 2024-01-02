d0de6cab5be88e23cd185a561de3ea370f585d00
Manan Tank <manantankm@gmail.com>
2024-01-02 23:53:38 +0530

    Content Fixes 5 (#74)
    * Fixes
    
    * Fix broken links
    
    * add API Keys
    
    * Base contracts
    
    * update
    
    * ThirdwebSDK unity
    
    * update
    
    * update

--- src/app/unity/sidebar.tsx
+++ src/app/unity/sidebar.tsx
@@ -302,6 +302,10 @@ export const sidebar: SideBar = {
 			href: "/unity/getting-started",
 		},
 		{
+			name: "ThirdwebSDK",
+			href: "/unity/ThirdwebSDK",
+		},
+		{
 			name: "Thirdweb Manager",
 			href: "/unity/thirdwebmanager",
 		},
