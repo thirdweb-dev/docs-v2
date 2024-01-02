56f785da37293e19fb821349f44aeecdc38e675c
Manan Tank <manantankm@gmail.com>
2024-01-02 18:57:19 +0530

    OG Images (#71)
    * test
    
    * updates
    
    * update

---
+++ src/app/contracts/opengraph-image.tsx
@@ -0,0 +1,10 @@
+import { openGraphImg } from "@og";
+
+export const runtime = "edge";
+
+export default function Image() {
+	return openGraphImg({
+		title: "thirdweb contracts",
+		icon: "contract",
+	});
+}
