4ee3fb4fb8719e39a23bac77df04668dc75b55e1
Manan Tank <manantankm@gmail.com>
2024-01-05 02:06:45 +0530

    fixes

--- src/components/Document/OpenSourceCard.tsx
+++ src/components/Document/OpenSourceCard.tsx
@@ -8,15 +8,17 @@ export function OpenSourceCard(props: {
 }) {
 	return (
 		<div className="my-4 rounded-lg border bg-b-800 p-4">
-			<div className="mb-2 text-lg font-semibold ">
+			<div className="mb-1 text-lg font-semibold ">
 				{props.title || "Open Source"}
 			</div>
-			<Paragraph className="mb-3 text-f-300">
+			<Paragraph className="mb-5 text-f-300">
 				{props.description ||
 					`${props.title} is open-source. You can view the source code and
 				contribute to it on GitHub`}
 			</Paragraph>
-			<GithubButtonLink href={props.href} />
+			<div className="flex">
+				<GithubButtonLink href={props.href} />
+			</div>
 		</div>
 	);
 }
