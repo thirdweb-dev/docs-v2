bb283dbcf42dc6e93fa9acd74d9c9c9d428accd5
samina <57885104+saminacodes@users.noreply.github.com>
2024-01-09 13:05:52 -0700

    moved API Keys to account (#96)
    * moved API Keys to account
    
    * added billing articles

---
+++ src/app/account/sidebar.tsx
@@ -0,0 +1,54 @@
+const accountSlug = "/account";
+import { SideBar } from "@/components/Layouts/DocLayout";
+
+export const sidebar: SideBar = {
+	name: "Account",
+	links: [
+		{
+			name: "Billing",
+			links: [
+				{
+					name: "Upgrade Plan",
+					href: `${accountSlug}/billing/upgrade-plan`,
+				},
+				{
+					name: "Manage Billing",
+					href: `${accountSlug}/billing/manage-billing`,
+				},
+			],
+		},
+		{
+			name: "API Keys",
+			links: [
+				{
+					name: "Overview",
+					href: `${accountSlug}/api-keys`,
+				},
+				{
+					name: "Create API Key",
+					href: `${accountSlug}/api-keys/create`,
+				},
+				{
+					name: "Use API Key",
+					href: `${accountSlug}/api-keys/use`,
+				},
+				{
+					name: "Edit enabled services",
+					href: `${accountSlug}/api-keys/edit-services`,
+				},
+				{
+					name: "Delete API Key",
+					href: `${accountSlug}/api-keys/delete`,
+				},
+				{
+					name: "Access Restrictions",
+					href: `${accountSlug}/api-keys/access`,
+				},
+				{
+					name: "FAQs",
+					href: `${accountSlug}/api-keys/faq`,
+				},
+			],
+		},
+	],
+};
