bf2ed60d9c18b08d204b639d2a9af210da862457
Manan Tank <manantankm@gmail.com>
2024-01-12 02:47:18 +0530

    Fix PDF link

--- src/components/Document/DocLink.tsx
+++ src/components/Document/DocLink.tsx
@@ -5,6 +5,7 @@ export function DocLink(props: {
 	href: string;
 	children: React.ReactNode;
 	className?: string;
+	isExternal?: boolean;
 }) {
 	return (
 		<Link
@@ -13,7 +14,9 @@ export function DocLink(props: {
 				"text-accent-500 hover:text-f-100 transition-colors",
 				props.className,
 			)}
-			target={props.href.startsWith("http") ? "_blank" : undefined}
+			target={
+				props.isExternal || props.href.startsWith("http") ? "_blank" : undefined
+			}
 		>
 			{props.children}
 		</Link>
