45f5e8c50019640e498cbab58b19f9aec3601b4d
Manan Tank <manantankm@gmail.com>
2024-01-23 14:41:15 +0530

    Fix search, fetch react-native docs from github

---
+++ src/lib/fetchJSON.ts
@@ -0,0 +1,16 @@
+import nodeFetch from "node-fetch";
+
+const fetchCache = new Map<string, Promise<any>>();
+
+export async function fetchJSON(url: string) {
+	if (fetchCache.has(url)) {
+		return fetchCache.get(url);
+	}
+
+	console.log(`Fetching ${url}`);
+
+	const fetchPromise = nodeFetch(url).then((res) => res.json());
+	fetchCache.set(url, fetchPromise);
+
+	return fetchPromise;
+}
