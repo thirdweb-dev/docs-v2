7ea684c295b4826ce91c14ab2f40f1745ba2b440
Phillip Ho <arcoraven@gmail.com>
2024-02-21 22:10:35 -0800

    feat: Engine cancel tx, webhook diagram, tx lifecycle (#199)
    * chore: Add details about cancelling txs
    
    * feat: Engine cancel tx, webhook diagram, tx lifecycle
    
    * fix build

--- src/app/engine/sidebar.tsx
+++ src/app/engine/sidebar.tsx
@@ -1,5 +1,4 @@
 import { SideBar } from "@/components/Layouts/DocLayout";
-import { InfraEngineIcon } from "@/icons";
 
 const engineSlug = "/engine";
 
@@ -10,91 +9,86 @@ export const sidebar: SideBar = {
 			name: "Overview",
 			href: "/engine",
 		},
-		{ separator: true },
-		// Engine
 		{
-			name: "Engine",
-			icon: <InfraEngineIcon />,
-			isCollapsible: false,
+			name: "Get Started",
+			href: `${engineSlug}/get-started`,
+		},
+		{
+			name: "Production Checklist",
+			href: `${engineSlug}/production-checklist`,
+		},
+		{
+			name: "Self-Host",
+			href: `${engineSlug}/self-host`,
+		},
+		{
+			name: "Features",
 			links: [
 				{
-					name: "Get Started",
-					href: `${engineSlug}/get-started`,
+					name: "Backend Wallets",
+					href: `${engineSlug}/features/backend-wallets`,
 				},
 				{
-					name: "Production Checklist",
-					href: `${engineSlug}/production-checklist`,
+					name: "Contracts",
+					href: `${engineSlug}/features/contracts`,
 				},
 				{
-					name: "Self-Host",
-					href: `${engineSlug}/self-host`,
+					name: "Permissions",
+					href: `${engineSlug}/features/permissions`,
 				},
 				{
-					name: "Features",
-					links: [
-						{
-							name: "Backend Wallets",
-							href: `${engineSlug}/features/backend-wallets`,
-						},
-						{
-							name: "Contracts",
-							href: `${engineSlug}/features/contracts`,
-						},
-						{
-							name: "Permissions",
-							href: `${engineSlug}/features/permissions`,
-						},
-						{
-							name: "Webhooks",
-							href: `${engineSlug}/features/webhooks`,
-						},
-						{
-							name: "Smart Wallets",
-							href: `${engineSlug}/features/smart-wallets`,
-						},
-						{
-							name: "Relayers",
-							href: `${engineSlug}/features/relayers`,
-						},
-						{
-							name: "Gasless Transactions",
-							href: `${engineSlug}/features/gasless-transactions`,
-						},
-					],
+					name: "Webhooks",
+					href: `${engineSlug}/features/webhooks`,
 				},
 				{
-					name: "Guides",
-					links: [
-						{ name: "Airdrop NFTs", href: `${engineSlug}/guides/airdrop-nfts` },
-						{ name: "NFT Checkout", href: `${engineSlug}/guides/nft-checkout` },
-						{
-							name: "Smart Wallets & Session Keys",
-							href: `${engineSlug}/guides/smart-wallets`,
-						},
-					],
+					name: "Cancelling Transactions",
+					href: `${engineSlug}/features/cancelling-transactions`,
 				},
 				{
-					name: "References",
-					links: [
-						{
-							name: "API Reference",
-							href: `${engineSlug}/references/api-reference`,
-						},
-						{
-							name: "Typescript SDK",
-							href: `${engineSlug}/references/typescript`,
-						},
-					],
+					name: "Smart Wallets",
+					href: `${engineSlug}/features/smart-wallets`,
 				},
 				{
-					name: "Security",
-					href: `${engineSlug}/security`,
+					name: "Relayers",
+					href: `${engineSlug}/features/relayers`,
 				},
 				{
-					name: "FAQ",
-					href: `${engineSlug}/faq`,
+					name: "Gasless Transactions",
+					href: `${engineSlug}/features/gasless-transactions`,
 				},
 			],
+		},
+		{
+			name: "Guides",
+			links: [
+				{ name: "Airdrop NFTs", href: `${engineSlug}/guides/airdrop-nfts` },
+				{ name: "NFT Checkout", href: `${engineSlug}/guides/nft-checkout` },
+				{
+					name: "Smart Wallets & Session Keys",
+					href: `${engineSlug}/guides/smart-wallets`,
+				},
+			],
+		},
+		{
+			name: "References",
+			links: [
+				{
+					name: "API Reference",
+					href: `${engineSlug}/references/api-reference`,
+				},
+				{
+					name: "Typescript SDK",
+					href: `${engineSlug}/references/typescript`,
+				},
+			],
+		},
+		{
+			name: "Security",
+			href: `${engineSlug}/security`,
+		},
+		{
+			name: "FAQ",
+			href: `${engineSlug}/faq`,
 		},
 	],
 };
