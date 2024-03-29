82f6d45f9818a2afc4d13811d47f008f0cb86f9e
Joaquim Verges <joaquim.verges@gmail.com>
2024-03-29 16:49:48 -0700

    v5 doc updates (#298)
    * updates
    
    * fix homepage link

---
+++ src/components/Document/V4Callout.tsx
@@ -0,0 +1,10 @@
+import { Callout } from ".";
+
+export function V4Callout() {
+	return (
+		<Callout variant="info" title="Connect v4 documentation">
+			This page includes code references for Connect SDK v4. We will be adding
+			v5 references for this section soon.
+		</Callout>
+	);
+}
