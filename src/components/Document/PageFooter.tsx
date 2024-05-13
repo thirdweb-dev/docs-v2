4c6a22bc98480d122e6d6459860311d6179f7846
Kien Ngo <kientngo@icloud.com>
2024-05-12 20:07:09 -0400

    Various DEVX issues update (#400)
    * Reduce gap between footer items
    
    * Update new dotnet icon
    
    * DEVX-1713 - Update code snippet for /account/api-keys/use
    
    ---------
    
    Co-authored-by: Kien Ngo <kien@cavies.xyz>

--- src/components/Document/PageFooter.tsx
+++ src/components/Document/PageFooter.tsx
@@ -59,7 +59,7 @@ function FooterLinkItem(props: {
 	icon: LucideIcon;
 }) {
 	return (
-		<li className="flex items-center gap-3 text-f-300">
+		<li className="flex items-center gap-2 text-f-300">
 			<div className="flex items-center gap-2 font-medium">
 				<props.icon className="size-5" />
 				<span>{props.prefix}</span>
